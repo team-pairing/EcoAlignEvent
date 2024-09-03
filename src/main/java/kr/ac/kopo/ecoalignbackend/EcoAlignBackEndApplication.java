eefc992b606b4288a9d00812a39bb7f8aa89b30c
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-09-03 11:33:38 +0900

    first commit

---
+++ src/main/java/kr/ac/kopo/ecoalignbackend/EcoAlignBackEndApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.ecoalignbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoAlignBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoAlignBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/kopo/ecoalignbackend/EcoAlignBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.ecoalignbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoAlignBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
