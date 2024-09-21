fc32be1a89ebcbfc223c9840ea054b6bbf2c79df
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-09-21 17:50:11 +0900

    email certification revised

--- src/main/java/kr/ac/kopo/ecoalignbackend/controller/MailController.java
+++ src/main/java/kr/ac/kopo/ecoalignbackend/controller/MailController.java
@@ -31,17 +31,18 @@ public class MailController {
     @ResponseBody
     @PostMapping("/send")
     public ResponseEntity<?> emailSend(@RequestBody MailDTO mailDTO) throws MessagingException {
-        String authCode = mailService.sendSimpleMessage(mailDTO.getEmail());
-        if (authCode == null) {
-            return ResponseEntity.badRequest().build(); // 실패 시 bad request
-        }
-        else {
-            Token token = jwtUtil.generateToken(authCode); // 인증 코드로 JWT 토큰 생성
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", token.getGrantType() + " " + token.getAccessToken());
-            Map<String, String> resultBody = new HashMap<>();
-            resultBody.put("check_number", authCode);
-            return ResponseEntity.status(200).headers(headers).body(resultBody); // Response body에 값을 반환
+        if (mailDTO.getEmail() == null) {
+            return ResponseEntity.badRequest().build(); // 사용자가 입력 없이 요청한 경우
+        } else {
+            String authCode = mailService.sendSimpleMessage(mailDTO.getEmail());
+            if (authCode == null) {
+                return ResponseEntity.internalServerError().build(); // 이메일 전송 실패 시
+            } else {
+                Token token = jwtUtil.generateToken(authCode); // 인증 코드로 JWT 토큰 생성
+                HttpHeaders headers = new HttpHeaders();
+                headers.set("Authorization", token.getGrantType() + " " + token.getAccessToken());
+                return ResponseEntity.ok().headers(headers).build(); // 이메일 전송 성공 시
+            }
         }
     }
 
@@ -52,12 +53,12 @@ public class MailController {
         String authCode = jwtUtil.extractSubject(token); // 헤더에 포함되어있는 토큰에서 인증 코드를 추출
         if (authCode != null && dto.getCheckNumber() != null){
             if (authCode.equals(dto.getCheckNumber())) {
-                return ResponseEntity.status(200).build(); // 인증에 성공했을 때
+                return ResponseEntity.ok().build(); // 인증에 성공했을 때
             } else {
-                return ResponseEntity.badRequest().build(); // 인증에 실패한 경우
+                return ResponseEntity.notFound().build(); // 인증에 실패한 경우
             }
         } else {
-            return ResponseEntity.noContent().build(); // 입력이 빈 경우
+            return ResponseEntity.badRequest().build(); // 입력이 빈 경우
         }
     }
 }
