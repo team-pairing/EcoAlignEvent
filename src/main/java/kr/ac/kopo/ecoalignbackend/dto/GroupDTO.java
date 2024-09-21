fdc962a363e11468aaa7311d2c3cc49301aa7835
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-09-21 19:56:43 +0900

    group entity, group dto

---
+++ src/main/java/kr/ac/kopo/ecoalignbackend/dto/GroupDTO.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.ecoalignbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupDTO {
+    private String id;
+    private String groupItem;
+}
