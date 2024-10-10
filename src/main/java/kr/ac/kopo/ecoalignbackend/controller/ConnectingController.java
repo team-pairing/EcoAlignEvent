a228476a9e800f0d3bf6f73075b3c9c0a25d461e
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-10-10 13:43:19 +0900

    호석오빠 수정

--- src/main/java/kr/ac/kopo/ecoalignbackend/controller/ConnectingController.java
+++ src/main/java/kr/ac/kopo/ecoalignbackend/controller/ConnectingController.java
@@ -10,7 +10,7 @@ public class ConnectingController implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 허용
-                .allowedOrigins("http://192.168.24.189:8081")  // 허용할 출처
+                .allowedOrigins("http://3.34.212.42:8090")  // 허용할 출처
                 .allowedMethods("GET", "POST")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 자격 증명 허용
