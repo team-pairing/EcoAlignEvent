febf2854da1cfb7ee4e6918aa8eca1afa674f326
KIMPROSMINJU <b.m1nj00371@gmail.com>
2024-10-10 13:56:31 +0900

    renew build jar

--- src/main/java/kr/ac/kopo/ecoalignbackend/EcoAlignBackEndApplication.java
+++ src/main/java/kr/ac/kopo/ecoalignbackend/EcoAlignBackEndApplication.java
@@ -1,6 +1,5 @@
 package kr.ac.kopo.ecoalignbackend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,7 +9,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class EcoAlignBackEndApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // .env 파일 로드 (암호화 키 관리 파일)
 		SpringApplication.run(EcoAlignBackEndApplication.class, args);
 	}
 
